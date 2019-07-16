9b344aef198ec9b779421d2e120251d27c5c3925
jinal <kapatel.j@brillio.com>
2019-07-16 12:24:23 +0530

    first commit

---
+++ test/server.spec.js
@@ -0,0 +1,10 @@
+const supertest = require('supertest');
+const expressApp = require('../server.js');
+
+const app = supertest(expressApp);
+describe('GET /', function() {
+  it('respond with hello world !!', function(done) {
+    //navigate to root and check the the response is "hello world"
+    app.get('/').expect('hello world !!', done);
+  });
+});
