73616e08ba211b237ddb61c8112f891ee8b7245a
Jinalben Nikunjbhai Ka Patel <jinal_p60@yahoo.com>
2022-10-13 19:58:15 -0400

    Update server.spec.js

--- test/server.spec.js
+++ test/server.spec.js
@@ -5,6 +5,6 @@ const app = supertest(expressApp);
 describe('GET /', function() {
   it('respond with hello world !!', function(done) {
     //navigate to root and check the the response is "hello world !!"
-    app.get('/').expect('hello world !!', done);
+    app.get('/').expect('hello world !!xxx', done);
   });
 });
